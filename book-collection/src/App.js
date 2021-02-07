bc809aab54fa93de9d14be7cc123fafc868b4d5f
Rakshit Patel <Patel26@student.mitt.ca>
2021-02-07 17:08:17 -0600

    Makes a local jeson server for the app and prints the data of json file in console.

--- book-collection/src/App.js
+++ book-collection/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    fetch('http://localhost:4000/books')
+    .then((response) => {return response.json()})
+    .then(data => console.log(data))
+  }
   render = () => {
     return (
       null
